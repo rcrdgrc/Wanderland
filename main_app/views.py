d4baaedd1dcba48bae532d9da7162eafd66a5f1d
rcrdgrc <ricktx04@gmail.com>
2019-06-17 11:02:12 -0500

    user login

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Landing Page</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return HttpResponse('<h1>This is the about page</h1>')
