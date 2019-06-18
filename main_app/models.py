5ca0064ed4ee902b42d75e2e05dfef1d311c39da
rcrdgrc <ricktx04@gmail.com>
2019-06-18 09:31:09 -0500

    added detail page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Trip(models.Model):
+    destination = models.CharField(max_length=100)
+    start_date = models.DateField('Start Date')
+    end_date = models.DateField('End Date')
+    budget = models.IntegerField()
+
+    def __str__(self):
+        return self.destination
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'trip_id': self.id})
